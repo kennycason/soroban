f010d535858b19a355a066ef13477a3fe9354573
Kenny Cason <kenneth.cason@gmail.com>
2016-09-30 22:11:48 -0700

    update install version

--- script/brew/soroban.rb
+++ script/brew/soroban.rb
@@ -1,13 +1,13 @@
 class Call < Formula
   desc "Soroban: A Command Line Calculator"
   homepage "https://github.com/kennycason/soroban"
-  url "http://search.maven.org/remotecontent?filepath=com/kennycason/soroban/1.0/soroban-1.0.jar"
+  url "http://search.maven.org/remotecontent?filepath=com/kennycason/soroban/1.1/soroban-1.1.jar"
   sha256 "38b71bd58d677fee3063bf3bfacfa1e3f90a842875d5fd02016f0c9adf8080fc"
 
   def install
-    libexec.install "soroban-1.0.jar"
-    bin.write_jar_script libexec/"soroban-1.0.jar", "soroban"
-    puts "Finished installing Soroban 1.0"
+    libexec.install "soroban-1.1.jar"
+    bin.write_jar_script libexec/"soroban-1.1.jar", "soroban"
+    puts "Finished installing Soroban 1.1"
   end
 
   def server_script(server_jar); <<-EOS.undent
